e3c08d9ac47076ddbfa108dd9c4298c66789de48
kongfl888 <kongfl888@outlook.com>
2020-06-22 09:40:33 +0800

    Reorganize the ipk folder.

--- scripts/copyipk.sh
+++ scripts/copyipk.sh
@@ -2,6 +2,8 @@
 # https://github.com/kongfl888/nanopi-openwrt
 
 mkdir -p ./r2srom/ipk/
+mkdir -p ./r2srom/ipk/generic/
+mkdir -p ./r2srom/ipk/packages/
 
 if [ `find friendlywrt-rk3328/friendlywrt/bin/packages/* -name "*adbyby*.ipk" | grep "adbyby" -c` -gt 0 ]; then
 	mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/*adbyby*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack adbyby ipk fail" >> ipklost.txt
@@ -23,15 +25,23 @@ if [ `find friendlywrt-rk3328/friendlywrt/bin/packages/* -name "*luci-app-syncdi
 	mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/luci-app-syncdial*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack syncdial ipk fail" >> ipklost.txt
 fi
 
+if [ `find friendlywrt-rk3328/friendlywrt/bin/targets/* -name "kmod-macvlan*.ipk" | grep "kmod-macvlan" -c` -gt 0 ]; then
+	mv -f friendlywrt-rk3328/friendlywrt/bin/targets/*/generic/packages/kmod-macvlan*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack kmod-macvlan ipk fail" >> ipklost.txt
+fi
+
 if [ `find friendlywrt-rk3328/friendlywrt/bin/packages/* -name "*luci-app-r2sflasher*.ipk" | grep "r2sflasher" -c` -gt 0 ]; then
 	mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/luci-app-r2sflasher*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack r2sflasher ipk fail" >> ipklost.txt
 fi
 
 if [ `find friendlywrt-rk3328/friendlywrt/bin/packages/* -name "*.ipk" | grep ".ipk" -c` -gt 0 ]; then
-	mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack all ipk fail 1" >> ipklost.txt
-    mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*.ipk ./r2srom/ipk/ >/dev/null 2>&1 || echo "pack all ipk fail 2" >> ipklost.txt
+	mv -f friendlywrt-rk3328/friendlywrt/bin/targets/*/generic/packages/*.ipk ./r2srom/ipk/generic/ >/dev/null 2>&1 || echo "pack generic ipk fail" >> ipklost.txt
+    mv -f friendlywrt-rk3328/friendlywrt/bin/packages/*/*/*.ipk ./r2srom/ipk/packages/ >/dev/null 2>&1 || echo "pack all ipk fail" >> ipklost.txt
 fi
 
 if [ `find ./r2srom/ipk/* -name "*.ipk" | grep ".ipk" -c` -eq 0 ]; then
     echo "1" > ./r2srom/ipk/noipk
+fi
+
+if [ -e ".current_config.mk" ]; then
+    cp -f .current_config.mk ./r2srom/
 fi
